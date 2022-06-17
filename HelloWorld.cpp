8a1db7c9823609ab7be6abb11c04c09b51790f79
sourav210295 <71113388+sourav210295@users.noreply.github.com>
2022-06-17 13:40:30 +0530

    Add files via upload

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -10,6 +10,13 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
-	return 0;
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
 }
