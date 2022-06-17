833e45d32ced9aae2583acb3d3a2897f643097c1
sourav210295 <71113388+sourav210295@users.noreply.github.com>
2022-06-17 13:49:17 +0530

    Add files via upload

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -9,14 +9,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int x, y;
-  int sum;
-  cout << "Type a number: ";
-  cin >> x;
-  cout << "Type another number: ";
-  cin >> y;
-  sum = x + y;
-  cout << "Sum is: " << sum;
-  return 0;
+int main()
+{
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
 }
